3eae8915e1f77d89e5a1b420cd5a5f319bc98a74
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-08-23 18:05:16 +0300

    field & constructor injection sample

--- HiltSample/app/src/main/java/com/bsrakdg/hiltsample/MainActivity.kt
+++ HiltSample/app/src/main/java/com/bsrakdg/hiltsample/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.bsrakdg.hiltsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+/*
+    Hilt can provide dependencies to other Android classes that have the @AndroidEntryPoint annotation:
+    Application (by using @HiltAndroidApp)
+    Activity
+    Fragment
+    View
+    Service
+    BroadcastReceiver
+ */
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // field injection
+    @Inject
+    lateinit var someClass: SomeClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(someClass.doAThing())
+        println(someClass.doOtherThing())
+    }
+}
+
+
+class SomeClass
+@Inject
+constructor(
+    private val someOtherClass: SomeOtherClass // constructor injection
+) {
+    fun doAThing(): String {
+        return "Look I did a thing!"
+    }
+
+    fun doOtherThing(): String {
+        return someOtherClass.doSomeOtherThing()
+    }
+}
+
+class SomeOtherClass
+@Inject
+constructor() {
+    fun doSomeOtherThing(): String {
+        return "Look I did some other thing!"
     }
 }
